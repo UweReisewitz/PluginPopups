60a86eb5612c0ecd3aec31c1822bb2a63967d7a0
Uwe Reisewitz <uwer@ordat.com>
2021-02-01 17:58:25 +0100

    Add project files.

---
+++ PluginPopups.WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Plugin.Popups;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace PluginPopups.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+
+
+            LoadApplication(new PluginPopups.App(new WpfInitializer()));
+        }
+
+    }
+
+    public class WpfInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterPopupNavigationService();
+        }
+    }
+}
---
+++ PluginPopups/App.xaml.cs
@@ -0,0 +1,33 @@
+using PluginPopups.ViewModels;
+using PluginPopups.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace PluginPopups
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
